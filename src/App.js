7b696d18502704e96edbe9be77a8ca7c5a6ee709
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-06-09 04:26:17 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCross, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        loading: true,
+        dataURL: "https://labs.bible.org/api/?passage=random",
+        bgColor: '',
+      };
+    }
+
+
+    componentDidMount() {
+      this.randomColor();
+      this.setState({
+        loading: false,
+        dataURL: "https://labs.bible.org/api/?passage=random",
+      });
+    }
+    
+    randomColor() {
+      const x = Math.floor(Math.random() * 256);
+      const y = Math.floor(Math.random() * 256);
+      const z = Math.floor(Math.random() * 256);
+      const bgColor = `rgb(${x},${y},${z})`;
+      this.setState({
+        bgColor,
+      });
+    }
+
+    handleClick = () => {
+      window.location.reload(false);
+    } 
+
+
+
+  render() {
+    const {loading} = this.state;
+    if (loading) return <div><FontAwesomeIcon icon={faSpinner} className="fa-pulse fa-5x" /></div>
+    document.body.style.backgroundColor = this.state.bgColor;
+
+    const styles = {
+      backgroundColor: this.state.bgColor,
+      float: "right",
+      border: "none"
+    }
+
+    const cross = {
+      color: this.state.bgColor,
+    }
+
+    return (
+      document.body.style.fontSize= "30px",
+      <div id="main">
+        <div id="verse-container">
+        <div id="wrap"><iframe id="verse" title="This is a unique title" style={{width: "100%"}}src={this.state.dataURL}></iframe></div>
+        <div id="flex-container">
+        <button type="button" className="btn btn-primary" onClick={this.handleClick} style={styles}>New verse</button>
+        <FontAwesomeIcon icon={faCross} style={cross} className="fa-2x" />
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
